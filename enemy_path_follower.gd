fcc2efe635d4008aa4bf07414fd9d305fea673a7
Robbin Ploeger <robbplo@gmail.com>
2024-07-20 11:03:29 +0200

    smooth enemy rotation

--- enemy_path_follower.gd
+++ enemy_path_follower.gd
@@ -12,6 +12,7 @@ const ALERT_SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 const APPROACH_DEADZONE = 5.0
 const WAIT_BEFORE_RETURNING = 1.0
+const ROTATION_SPEED = TAU
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 
@@ -31,20 +32,23 @@ func _physics_process(delta: float) -> void:
 		State.ALERTED:
 			if nav.is_target_reached():
 				state = State.ALERTED_WAITING
-				await get_tree().create_timer(WAIT_BEFORE_RETURNING).timeout
-				state = State.RETURNING
-				nav.target_position = get_parent().global_position
+				#await get_tree().create_timer(WAIT_BEFORE_RETURNING).timeout
+				#return_to_path()
 			else:
-				move_toward_target()
+				move_toward_target(delta)
 		State.RETURNING:
 			if nav.is_target_reached():
 				follow_path()
 			else:
-				move_toward_target()
+				move_toward_target(delta)
 
 func alert(target: Vector2):
 	nav.target_position = target
 	state = State.ALERTED
+
+func return_to_path():
+	state = State.RETURNING
+	nav.target_position = get_parent().global_position
 
 func follow_path():
 	position = Vector2.ZERO
@@ -52,9 +56,17 @@ func follow_path():
 	rotation = 0
 
 
-func move_toward_target():
+func move_toward_target(delta):
+	if not nav.is_target_reachable():
+		# TODO: pick valid nearby location
+		print("target not reachable")
+		return return_to_path()
 	var direction = global_position.direction_to(nav.get_next_path_position())
-	velocity = direction * ALERT_SPEED
-	look_at(nav.get_next_path_position())
-	move_and_slide()
+	var target_angle = direction.angle()
+	# Scale rotation to size of angle
+	var rot_scale = abs(angle_difference(global_rotation, target_angle))
+	var delta_rotation = ROTATION_SPEED * delta * rot_scale
+	global_rotation = rotate_toward(global_rotation, target_angle, delta_rotation )
 
+	velocity = direction * ALERT_SPEED
+	move_and_slide()
