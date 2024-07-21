bac55a5989bd99ac8498878515cf78ba7a689fde
Robbin Ploeger <robbplo@gmail.com>
2024-07-21 17:47:03 +0200

    player is eye looking at mouse

--- player.gd
+++ player.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 class_name Player
 
 const SPEED := 300.0
-const THROW_SPEED = 900.0
+const THROW_SPEED := 900.0
+const EYE_MAX_MOVEMENT := Vector2(80, 60)
+const EYE_MAX_DISTANCE := Vector2(600.0, 300.0)
 const POTION_SCENE := preload("res://potion.tscn")
 
 func _physics_process(_delta: float) -> void:
 	var input_vector := Input.get_vector("left", "right", "up", "down")
 	velocity = input_vector * SPEED
 	move_and_slide()
-	look_at(get_global_mouse_position())
+	_eye_look_at(get_global_mouse_position())
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -24,3 +26,11 @@ func throw_potion(target: Vector2):
 
 func die():
 	queue_free()
+
+func _eye_look_at(target: Vector2):
+	var distance = (target - global_position).clamp(-EYE_MAX_DISTANCE, EYE_MAX_DISTANCE)
+	var direction = global_position.direction_to(target)
+	var movement_scale = (distance / EYE_MAX_DISTANCE) * direction.sign()
+	$EyeIris.position = movement_scale * direction * EYE_MAX_MOVEMENT
+
+
